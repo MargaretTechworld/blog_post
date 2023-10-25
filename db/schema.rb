94038d9b0eac32d350eb0d3c2dc348e55598fe70
MargaretTechworld <margarettechworld@gmail.com>
2023-10-25 16:32:29 +0000

    Update foreign key name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_130612) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_163103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,15 +34,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_130612) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
     t.string "title"
     t.string "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.bigint "user_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,5 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_130612) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
