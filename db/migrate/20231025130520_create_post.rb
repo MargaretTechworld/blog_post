2e6b0313c17335785c6898eebf5e86336debe900
MargaretTechworld <margarettechworld@gmail.com>
2023-10-25 14:46:24 +0000

    Create tables according to ER diagram with relationships

---
+++ db/migrate/20231025130520_create_post.rb
@@ -0,0 +1,13 @@
+class CreatePost < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.integer :author_id
+      t.string :title
+      t.string :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
