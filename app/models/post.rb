2c5867fbdb7c362c27c257a1d871147590e4a76d
MargaretTechworld <margarettechworld@gmail.com>
2023-10-27 20:36:25 +0000

    Add private methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,13 @@ class Post < ApplicationRecord
   has_many :likes
   after_save :update_post_counter
 
-  def update_post_counter
-    author.increment!(:posts_counter)
-  end
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
