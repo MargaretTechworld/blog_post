df0b17d710a07236dada37ea7a6f519f830fdf55
MargaretTechworld <margarettechworld@gmail.com>
2023-11-01 23:32:43 +0000

    Add test for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:post) { Post.new(author: user, title: 'Hello', text: 'This is my first post') }
+
+  describe 'GET /users/:user_id/posts' do
+    it 'renders the posts index page' do
+      get "/users/:#{user.id}/posts"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '/users/:user_id/posts/:id' do
+    it 'renders the post show page' do
+      get "/users/:#{user.id}/posts/:#{post.id}"
+      expect(response).to have_http_status(200)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
+  describe 'GET /' do
+    it 'renders the root page' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users' do
+    it 'renders the users index page' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+
+  describe 'GET /users/:id' do
+    it 'renders the user show page' do
+      get "/users/:#{user.id}"
+      expect(response).to have_http_status(200)
+    end
   end
 end
