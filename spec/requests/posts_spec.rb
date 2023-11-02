4010d70cdbae8d6f102b8f8ab3854feb32d6e15b
MargaretTechworld <margarettechworld@gmail.com>
2023-11-02 00:09:35 +0000

    Add controllers test as per requirment

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,10 +11,39 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
+  describe 'GET /users/:user_id/posts' do
+    it 'renders the post index page with correct template' do
+      get "/users/:#{user.id}/posts"
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:user_id/posts' do
+    it 'renders the post index page with correct placeholder text' do
+      get "/users/:#{user.id}/posts"
+      expect(response.body).to include('Post 1')
+    end
+  end
+
   describe '/users/:user_id/posts/:id' do
     it 'renders the post show page' do
       get "/users/:#{user.id}/posts/:#{post.id}"
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'renders the post show page with correct template' do
+      get "/users/:#{user.id}/posts/:#{post.id}"
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'renders the post show page with correct placeholder text' do
+      get "/users/:#{user.id}/posts/:#{post.id}"
+      expect(response.body).to include('All Posts by User')
+    end
+  end
+
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+
   describe 'GET /users' do
     it 'renders the users index page' do
       get '/users'
@@ -16,11 +17,37 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+  describe 'GET /users' do
+    it 'renders the user index page with correct template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+  end
+  describe 'GET /users' do
+    it 'renders the user index page with correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('User 1')
+    end
+  end
 
-  describe 'GET /users/:id' do
+   describe 'GET /users/:id' do
     it 'renders the user show page' do
       get "/users/:#{user.id}"
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'renders the user show page with correct template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'renders the user show page with correct placeholder text' do
+      get "/users/#{user.id}"
+      expect(response.body).to include('All User')
     end
   end
 end
