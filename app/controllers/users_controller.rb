47d00aa595dab7b480afedf4756b37a4ca2b2d7d
MargaretTechworld <margarettechworld@gmail.com>
2023-11-04 19:33:50 +0000

    Fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   layout 'application'
   def index
-  @users = User.all 
+    @users = User.all
   end
 
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def truncate(text)
-        text.length > 200 ? "#{text[0, 200]}..." : text
-    end
+  def truncate(text)
+    text.length > 200 ? "#{text[0, 200]}..." : text
+  end
 end
