b7d336c04325e52bc9a334d1308ffd89bc280d7e
MargaretTechworld <margarettechworld@gmail.com>
2023-11-07 17:13:16 +0000

    Updated old test files

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,52 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let(:user) { User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
-  describe 'GET /' do
-    it 'renders the root page' do
-      get '/'
+  let(:valid_attributes) { { name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0 } }
+  describe 'GET /index' do
+    before { User.create!(valid_attributes) }
+
+    it 'returns the correct response status' do
+      get users_url
       expect(response).to have_http_status(200)
     end
-  end
 
-  describe 'GET /users' do
-    it 'renders the users index page' do
-      get '/users'
-      expect(response).to have_http_status(200)
+    it 'response body includes correct placeholder text' do
+      get users_url
+      expect(response.body).to include('Tom')
     end
-  end
 
-  describe 'GET /users' do
-    it 'renders the user index page with correct template' do
-      get '/users'
+    it 'renders the correct template' do
+      get users_path
       expect(response).to render_template(:index)
     end
   end
-  describe 'GET /users' do
-    it 'renders the user index page with correct placeholder text' do
-      get '/users'
-      expect(response.body).to include('User 1')
-    end
-  end
 
-  describe 'GET /users/:id' do
-    it 'renders the user show page' do
-      get "/users/:#{user.id}"
+  describe 'GET /show' do
+    let(:user) { User.create!(valid_attributes) }
+
+    it 'returns the correct response status' do
+      get user_path(user)
       expect(response).to have_http_status(200)
     end
-  end
 
-  describe 'GET /users/:id' do
-    it 'renders the user show page with correct template' do
-      get "/users/#{user.id}"
+    it 'renders the correct template' do
+      get user_path(user)
       expect(response).to render_template(:show)
     end
-  end
 
-  describe 'GET /users/:id' do
-    it 'renders the user show page with correct placeholder text' do
-      get "/users/#{user.id}"
-      expect(response.body).to include('All User')
+    it 'response body includes correct placeholder text' do
+      get user_path(user)
+      expect(response.body).to include('Tom')
     end
   end
 end
