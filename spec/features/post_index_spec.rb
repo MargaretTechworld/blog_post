b8cb31f14435ade59b1072f6f46f6f8a60a0721e
MargaretTechworld <margarettechworld@gmail.com>
2023-11-07 20:12:34 +0000

    Add Tests that asserts you can see the first comments on a post and Tests that redirects to the post's show page when you click on a post.

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -20,10 +20,18 @@ RSpec.describe 'Posts', type: :feature do
     it 'shows the number of posts for each user' do
       expect(page).to have_content("Number of Posts: #{user.posts_counter}")
     end
-
+    context 'Click' do
+      let(:first_recent_post) { user.recent_post[0] }
+      it "redirects me to that post's show page when I click on a post" do
+        if first_recent_post
+          click_link first_recent_post.title
+          expect(page).to have_current_path(user_post_path(user, first_recent_post))
+        end
+      end
+    end
     context 'when a recent post exists' do
       let(:first_recent_post) { user.recent_post[0] }
-
+      let(:first_recent_comment) { post.recent_comments[0] }
       it "shows the recent post's title" do
         expect(page).to have_content(first_recent_post.title) if first_recent_post
       end
@@ -43,7 +51,9 @@ RSpec.describe 'Posts', type: :feature do
       it 'shows the number of comments for the recent post' do
         expect(page).to have_content("Comments: #{first_recent_post.comments_counter}") if first_recent_post
       end
-
+      it 'asserts you can see the first comments on a post' do
+        expect(page).to have_content(first_recent_comment) if first_recent_post
+      end
       it 'shows the number of likes for the recent post' do
         expect(page).to have_content("Likes: #{first_recent_post.likes_counter}") if first_recent_post
       end
