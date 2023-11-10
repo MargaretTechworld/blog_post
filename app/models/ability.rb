d224cf7b8d993f1dcc49f3ec4ea5c37e12caaf85
Berhanu Baye <berhan.baye@gmail.com>
2023-11-10 23:52:56 +0300

    Update ability add permissions for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+ user ||= User.new # Guest user
 
- 
+    can :read, :all
+
+    if user.role == "admin"
+      can :manage, :all
+    else
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, user_id: user.id
+    end
   end
 end
