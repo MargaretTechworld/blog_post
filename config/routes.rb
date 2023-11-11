0ec2fac4e8043c1f1b845a59bdf1a7ffdee65a0f
MargaretTechworld <margarettechworld@gmail.com>
2023-11-11 18:40:25 +0000

    Created routes for Api

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
 }
   root "users#index"
+  namespace :api do
+  namespace :v1 do
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show, :new, :create, :destroy] do
   resources :comments, only: [:create, :destroy ] 
   resources :likes, only: [:create, :destroy] 
+end
+end
 end
 end
 end
